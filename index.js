2acf6fe1b566b4e229956c62e59f8da51ce040f2
Demarcus Hill <demarcushill20@gmail.com>
2023-03-03 00:23:09 -0600

    Finished sprint 1

--- index.js
+++ index.js
@@ -209,17 +209,19 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 (not auto tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
+artists[8].name = "Vincent van Gogh";
 
+console.log(artists); // output: the corrected dataset with Vincent van Gogh's name fixed
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -230,8 +232,9 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artists, index) {
+  const artist = artists[index];
+  return `the artist at index ${artist.id} is ${artist.name}`;
 }
 
 
@@ -244,8 +247,17 @@ Use listOfNames to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(artists) {
+  // Create a new array with the same length as the artists array
+  const names = new Array(artists.length);
+  
+  // Loop over the artists array and update the names array
+  for (let i = 0; i < artists.length; i++) {
+    names[i] = artists[i].name;
+  }
+  
+  // Return the names array
+  return names;
 }
 
 
@@ -259,8 +271,13 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(artists, index) {
+  // create a new array that is a copy of the parameter array
+  const artistsCopy = [...artists];
+  // remove an artist from the copied array at the desired index
+  artistsCopy.splice(index, 1);
+  // return the resulting copied array
+  return artistsCopy;
 }
 
 
@@ -281,9 +298,35 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(artistsArr, name, years, genre, nationality, bio) {
+  // Create a shallow copy of the array
+  const copiedArr = [...artistsArr];
+
+  // Create a new object with the given properties
+  const newArtist = {
+    name,
+    years,
+    genre,
+    nationality,
+    bio,
+  };
+
+  // Add the new object to the copied array
+  copiedArr.push(newArtist);
+
+  // Return the copied array with the new object added
+  return copiedArr;
 }
+
+const updatedArtists = addArtist(
+  artists,
+  'Demarcus Hill',
+  '1991 - current day',
+  'Web Design',
+  'African-American',
+  'I am a student at Bloomtech University'
+);
+
 
 
 
@@ -295,8 +338,14 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(artists) {
+  // Use filter method to filter out artists with less than or equal to 100 paintings
+  const manyPaintings = artists.filter(artist => artist.paintings > 100);
+  
+  // Use map method to return an array with names of remaining artists
+  const names = manyPaintings.map(artist => artist.name);
+  
+  return names;
 }
 
 
@@ -311,8 +360,17 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(artists, name) {
+  // Loop through the artists array
+  for (let i = 0; i < artists.length; i++) {
+    // Check if the name of the current artist matches the input name
+    if (artists[i].name === name) {
+      // If so, return the artist's bio
+      return artists[i].bio;
+    }
+  }
+  // If no artist is found with the input name, return null
+  return null;
 }
 
 
@@ -327,8 +385,14 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(artistArr, nationality) {
+  // Filter artists by nationality and exclude artists with multiple nationalities
+  const filteredArtists = artistArr.filter(artist => artist.nationality === nationality && artist.nationality.split(',').length === 1);
+
+  // Map over the filtered array to only return artist names
+  const artistNames = filteredArtists.map(artist => artist.name);
+
+  return artistNames;
 }
 
 
